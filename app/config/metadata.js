601993dd59de62c9f62be004824ee7dcb0978e2a
Vincent R <vincent.r@maisondunet.com>
2018-03-08 09:18:37 +0100

    Add config file for SEO

---
+++ app/config/metadata.js
@@ -0,0 +1,15 @@
+module.exports = {
+	title: "WhaTT for now",
+	description: "WhaTT for now ?",
+	url: "https://www.whattfornow.fr/",
+	language: "fr",
+	language_iso: "fr_FR",
+	thumbnail: {
+		width: 1200,
+		height: 630,
+		url: "https://www.whattfornow.fr/assets/thumbnail.jpg"
+	},
+	keywords: 'whatt for now, whatt, whatt annecy, velo annecy',
+	author: "Maison du Net",
+	copyright: "© WhaTT 2018",
+}
--- app/config/webpack.dev.js
+++ app/config/webpack.dev.js
@@ -2,51 +2,52 @@ var path = require('path')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 var HTMLWebpackPlugin = require('html-webpack-plugin')
 var CopyWebpackPlugin = require('copy-webpack-plugin')
+var metadata = require('./metadata')
 
 module.exports = {
-    entry: './app/main.js',
-    output: {
-        path: path.resolve('app', 'dist'),
-        filename: 'app.bundle.js'
-    },
-    resolve: {
-        alias: {
-            '@': path.resolve('app', 'components')
-        }
-    },
-    devtool: 'cheap-module-eval-source-map',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'babel-loader'
-            },
-            {
-                test: /\.(s?)css$/,
-                use: ExtractTextPlugin.extract({
-                    use: [
-                        {
-                            loader: 'css-loader'
-                        },
-                        {
-                            loader: 'sass-loader'
-                        }
-                    ],
-                    fallback: 'style-loader'
-                })
-            },
-            {
-                test: /\.(png|jpg|gif|svg)$/,
-                use: [
-                  {
-                    loader: 'url-loader',
-                    options: {
-                      limit: 8192
-                    }
-                  }
-                ]
-            },
+	entry: './app/main.js',
+	output: {
+		path: path.resolve('app', 'dist'),
+		filename: 'app.bundle.js'
+	},
+	resolve: {
+		alias: {
+			'@': path.resolve('app', 'components')
+		}
+	},
+	devtool: 'cheap-module-eval-source-map',
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules|bower_components)/,
+				loader: 'babel-loader'
+			},
+			{
+				test: /\.(s?)css$/,
+				use: ExtractTextPlugin.extract({
+					use: [
+						{
+							loader: 'css-loader'
+						},
+						{
+							loader: 'sass-loader'
+						}
+					],
+					fallback: 'style-loader'
+				})
+			},
+			{
+				test: /\.(png|jpg|gif|svg)$/,
+				use: [
+					{
+						loader: 'url-loader',
+						options: {
+							limit: 8192
+						}
+					}
+				]
+			},
 			{
 				test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
 				use: [
@@ -59,31 +60,32 @@ module.exports = {
 						},
 					}
 				]
+			},
+		]
+	},
+	plugins: [
+		new ExtractTextPlugin('./style.css'),
+		new HTMLWebpackPlugin({
+			filename: 'index.html',
+			template: './app/index.html',
+			hash: true,
+			inject: true,
+			metadata
+		}),
+		new CopyWebpackPlugin([
+			{
+				from: path.resolve('app', 'static'),
+				to: 'assets/'
 			}
-        ]
-    },
-    plugins: [
-        new ExtractTextPlugin('./style.css'),
-        new HTMLWebpackPlugin({
-            filename: 'index.html',
-            template: './app/index.html',
-            hash: true,
-            inject: true
-        }),
-        new CopyWebpackPlugin([
-            {
-                from: path.resolve('app', 'static'),
-                to: 'assets/'
-            }
-        ])
-    ],
-    devServer: {
-        contentBase: path.resolve('app'),
-        watchContentBase: true,
-        port: 3000,
-        hot: true,
-        inline: true,
-        noInfo: true,
-        open: true
-    }
+		])
+	],
+	devServer: {
+		contentBase: path.resolve('app'),
+		watchContentBase: true,
+		port: 4000,
+		hot: true,
+		inline: true,
+		noInfo: true,
+		open: true
+	}
 }
